6871f8ce9f55824aaa776f2ee5d98f231459c649
Rose <120544840+Rose-my@users.noreply.github.com>
2024-08-11 11:57:28 +0900

    fix: 배포에러

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
+/// <reference types="node" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ include: "**/*.svg" }), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
