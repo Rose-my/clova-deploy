ffb1249f13cf82f0411e4ce8940b2813f16df81b
Rose <120544840+Rose-my@users.noreply.github.com>
2024-09-12 15:48:53 +0900

    design: delete Container position

--- frontend/src/pages/search/styles.ts
+++ frontend/src/pages/search/styles.ts
@@ -3,10 +3,7 @@ import styled from "styled-components";
 export const Container = styled.section`
   display: flex;
   flex-direction: column;
-  position: relative;
   width: 100%;
   height: 100vh;
-
-  /* padding: 4rem 2rem; */
   background-color: #f9f9f9;
 `;
