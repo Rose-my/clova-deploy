7876cf4ef1b92a8715c40aec9b32e77701a6484f
Rose <120544840+Rose-my@users.noreply.github.com>
2024-10-04 09:08:42 +0900

    feat: myupload 페이지 props 적용

--- frontend/src/pages/myupload/one/index.tsx
+++ frontend/src/pages/myupload/one/index.tsx
@@ -6,11 +6,20 @@ import * as O from "./styles";
 export default function index() {
   const location = useLocation();
   const { lostImgUrl } = location.state as { lostImgUrl: File };
-
+  const { losttime, lostdate, description, title, moredesc, founded, category, getwhere, nowwhere } = location.state;
   return (
     <O.Container>
       <O.Image src={URL.createObjectURL(lostImgUrl)} alt="LostItem" />
-      <Item />
+      <Item
+        lostdate={lostdate}
+        losttime={losttime}
+        description={description}
+        title={title}
+        moredesc={moredesc}
+        founded={founded}
+        getwhere={getwhere}
+        nowwhere={nowwhere}
+      />
       <Footer />
     </O.Container>
   );
